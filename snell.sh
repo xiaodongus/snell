023a33adef122e55a1cce7e2a21d0d2daef66240
曹润 <caorun@qq.com>
2019-02-18 18:05:57 +0800

    1

---
+++ snell.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+yum install unzip
+cd ～/
+wget --no-check-certificate -O snell.zip https://github.com/surge-networks/snell/releases/download/1.0.1/snell-server-v1.0.1-linux-amd64.zip
+unzip snell.zip
+rm -f snell.zip
+chmod +x snell-server
+cd /etc/systemd/system
+
+cat > snell.service<<-EOF
+[Unit]
+Description=Snell Server
+After=network.target
+
+[Service]
+ExecStart=/root/snell-server
+Restart=on-failure
+RestartSec=1s
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl daemon-reload
+systemctl start snell
+systemctl restart snell
+cat /snell-server.conf
